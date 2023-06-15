0229f998d5694584d3127e9c57e90c511fb89e59
urmilachikhalge <urmilachikhalge18@gmail.com>
2023-06-15 21:05:02 +0530

    First Commit

---
+++ testCases/test_file3.py
@@ -0,0 +1,11 @@
+class Test_py03:
+
+    def test_sum_006(self):
+        a = 8
+        b = 8
+        sum = a + b
+        print("sum -- >" + str(sum))
+        if sum == 16:
+            assert True
+        else:
+            assert False
